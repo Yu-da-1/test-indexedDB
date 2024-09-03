e4e76be86044bc1df1914ab3321d99a1a4681615
Yu-da-1 <yudai.ishi.0704@gmail.com>
2024-09-03 14:29:52 +0900

    add verify for signature

--- crypto.js
+++ crypto.js
@@ -1,4 +1,5 @@
 // ECDSAの鍵をgenerateKeyモジュールを使って生成
+const { error } = require("console");
 const { setupIndexedDB, saveKeyToIndexedDB, getKeyFromIndexedDB } = require("./db");
 
 // false or trueで非エクスポートするかどうかを設定可能
@@ -12,7 +13,7 @@ async function generateNonExtractableKey() {
         ["sign"]
     );
     const publicKey = await crypto.subtle.exportKey("spki", keyPair.publicKey);
-    console.log('PublicKey:', Buffer.from(publicKey).toString('hex'));
+    //console.log('PublicKey:', Buffer.from(publicKey).toString('hex'));
     return keyPair;
 }
 
@@ -31,17 +32,51 @@ async function generateSignature(keyPair, data) {
     return signature;
 }
 
+// 鍵の保存
 async function saveGenerateKey(key) {
     const db = await setupIndexedDB();
     await saveKeyToIndexedDB(db, key);
     console.log('Key saved to IndexedDB');
 }
 
+// 鍵の読み込み
 async function loadKeyFromDB(id) {
     const db = await setupIndexedDB();
     const storedKey = await getKeyFromIndexedDB(db, id);
-    console.log('Key loaded from IndexedDB:', storedKey);
+    //console.log('Key loaded from IndexedDB:', storedKey);
     return storedKey;
 }
 
-module.exports = {generateNonExtractableKey, generateSignature, saveGenerateKey, loadKeyFromDB };
+// 署名の検証
+async function verifySignature(exportedPublicKey, signature, message) {
+    try {
+        // publickeyのインポート
+        const importPublicKey = await crypto.subtle.importKey(
+            "spki",
+            exportedPublicKey,
+            {
+                name: "ECDSA",
+                namedCurve: "P-256"
+            },
+            true,
+            ["verify"]
+        );
+
+        //署名を検証
+        const isValid = await crypto.subtle.verify(
+            {
+                name: "ECDSA",
+                hash: {name: "SHA-256"},
+            },
+            importPublicKey,
+            signature,
+            new TextEncoder().encode(message)
+        );
+        return isValid;
+    } catch (error) {
+        console.error('署名の検証中にエラー:', error);
+        throw error;
+    }
+}
+
+module.exports = {generateNonExtractableKey, generateSignature, saveGenerateKey, loadKeyFromDB, verifySignature };
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const readline = require('readline');
-const { generateNonExtractableKey, generateSignature, saveGenerateKey, loadKeyFromDB } = require('./crypto');
+const { generateNonExtractableKey, generateSignature, saveGenerateKey, loadKeyFromDB, verifySignature } = require('./crypto');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -9,21 +9,29 @@ const rl = readline.createInterface({
 async function main() {
     try {
         // ECDSA鍵の生成
-        const key = await generateNonExtractableKey();
+        const keyPair = await generateNonExtractableKey();
         console.log('Success!');
 
-        await saveGenerateKey(key);
+        await saveGenerateKey(keyPair);
+
+        //公開鍵をエクスポート
+        const exportedPublicKey = await crypto.subtle.exportKey("spki", keyPair.publicKey);
+        console.log('PublicKey (exported:', Buffer.from(exportedPublicKey).toString('hex'));
 
         // メッセージの入力を促す
         rl.question('Enter message: ', async (message) => {
             try {
                 //保存した鍵をindexedDBから取得
                 const storedKey = await loadKeyFromDB(1);
-                console.log('Key loaded from IndexedDB:', storedKey);
+                //console.log('Key loaded from IndexedDB:', storedKey);
 
                 // メッセージに対して署名を生成
                 const signature = await generateSignature(storedKey.key, message);
                 console.log('Signature:', Buffer.from(signature).toString('hex'));
+
+                // 署名の検証
+                const isValid = await verifySignature(exportedPublicKey, signature, message);
+                console.log('Signature is valid:', isValid);
             } catch (error) {
                 console.error('署名の生成中にエラー:', error);
             } finally {
